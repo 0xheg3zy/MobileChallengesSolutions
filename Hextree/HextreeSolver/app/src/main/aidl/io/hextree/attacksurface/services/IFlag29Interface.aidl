7d07e1209ac0df469bf4568d05d85deeb8f37790
0xheg3zy <aliomar91777@gmail.com>
2025-03-25 22:52:31 +0200

    renamed

---
+++ Hextree/HextreeSolver/app/src/main/aidl/io/hextree/attacksurface/services/IFlag29Interface.aidl
@@ -0,0 +1,9 @@
+package io.hextree.attacksurface.services;
+
+interface IFlag29Interface {
+
+     String init();
+     void authenticate(String str);
+     void success();
+
+}
---
+++ Hextree/HextreeSolver/app/src/main/java/io/hextree/androidtest/MyReceiver2.java
@@ -0,0 +1,17 @@
+package io.hextree.androidtest;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver2 extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+//        Log.d("DATA : ", String.valueOf(intent.getStringExtra("flag")));
+//        throw new UnsupportedOperationException("Not yet implemented");
+
+    }
+}
